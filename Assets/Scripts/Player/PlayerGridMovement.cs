093cc6372fe7bb920cc1b51c36790ade2ef5278e
kcbt123 <36097630+kcbt123@users.noreply.github.com>
2021-04-18 14:29:08 +0700

    Update Programmatic Movement + Flower Placement Code

--- Assets/Scripts/Player/PlayerGridMovement.cs
+++ Assets/Scripts/Player/PlayerGridMovement.cs
@@ -18,6 +18,9 @@ public class PlayerGridMovement : MonoBehaviour
     [SerializeField]
     private Tilemap flowerTilemap;
 
+    [SerializeField]
+    private Tile replacementTile;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -49,14 +52,84 @@ public class PlayerGridMovement : MonoBehaviour
             }
         }
 
+        // Test
+        // Log ô đang đứng + Xóa ô hoa
         if (Input.GetKeyDown(KeyCode.Space)) 
         {
-            Debug.Log("Key pressed");
-            Debug.Log(string.Format("Current tile: [{0}, {1}]", movePoint.position.x, movePoint.position.y));
-            var tilePos = flowerTilemap.WorldToCell(movePoint.position);
-            flowerTilemap.SetTile(tilePos, null);
+            RemoveFlower(); 
         }
 
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Debug.Log("Begin to move");
+            StartCoroutine(MoveUpAfter(2f));
+        }
+    }
 
+    IEnumerator MoveUpAfter(float x)
+    {
+        yield return new WaitForSeconds(x);
+        MoveLeftSingle();
+        Debug.Log("Done moving");
+    }
+
+    /** ===== MARK: - Move By Code Functions ===== */
+    void MoveUpSingle()
+    {
+        Vector3 moveVector = new Vector3(0f, 1f, 0f);
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, movePoint.position) <= .5f)
+        {
+            if (!Physics2D.OverlapCircle(movePoint.position + moveVector, .2f, whatStopsMovement))
+            {
+                 movePoint.position += moveVector;
+            }
+        }
+    }
+    void MoveDownSingle()
+    {
+        Vector3 moveVector = new Vector3(0f, -1f, 0f);
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, movePoint.position) <= .5f)
+        {
+            if (!Physics2D.OverlapCircle(movePoint.position + moveVector, .2f, whatStopsMovement))
+            {
+                 movePoint.position += moveVector;
+            }
+        }
+    }
+
+    void MoveLeftSingle()
+    {
+        Vector3 moveVector = new Vector3(-1f, 0f, 0f);
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, movePoint.position) <= .5f)
+        {
+            if (!Physics2D.OverlapCircle(movePoint.position + moveVector, .2f, whatStopsMovement))
+            {
+                 movePoint.position += moveVector;
+            }
+        }
+    }
+
+    void MoveRightSingle()
+    {
+        Vector3 moveVector = new Vector3(1f, 0f, 0f);
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, movePoint.position) <= .5f)
+        {
+            if (!Physics2D.OverlapCircle(movePoint.position + moveVector, .2f, whatStopsMovement))
+            {
+                 movePoint.position += moveVector;
+            }
+        }
+    }
+
+    /** ===== MARK: - Remove flower by code ===== */
+    void RemoveFlower() 
+    {
+        Debug.Log(string.Format("Current tile: [{0}, {1}]", movePoint.position.x, movePoint.position.y));
+        var tilePos = flowerTilemap.WorldToCell(movePoint.position);
+        flowerTilemap.SetTile(tilePos, replacementTile);
     }
 }
