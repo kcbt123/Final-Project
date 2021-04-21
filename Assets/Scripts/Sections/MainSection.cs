62ffe68f37c51f7eb372c6caba1a6e4c573ff27e
kcbt123 <36097630+kcbt123@users.noreply.github.com>
2021-04-21 16:51:08 +0700

    Update Add + Delete functions for Code Blocks in section

---
+++ Assets/Scripts/Sections/MainSection.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainSection : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject codeBlockPrefab;
+
+    private List<GameObject> listCodeBlock;
+
+    private VerticalLayoutGroup verticalLayoutGroup;
+
+    //private List<CodeBlock> listCodeBlocks = new List<CodeBlock>(5);    // Start is called before the first frame update
+    void Start()
+    {
+        verticalLayoutGroup = transform.GetComponent<VerticalLayoutGroup>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Debug.Log("Adding new code block");
+            GameObject newCodeBlock = Instantiate(codeBlockPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            newCodeBlock.transform.SetParent(this.transform);
+        } else if (Input.GetKeyDown(KeyCode.H))
+        {
+            if (transform.childCount > 0) {
+                Debug.Log("Deleting a code block");
+                GameObject lastCodeBlock = transform.GetChild(transform.childCount - 1).gameObject;
+                Destroy(lastCodeBlock);
+            } else {
+                Debug.Log("No child left");
+            }
+
+        }
+    }
+}
