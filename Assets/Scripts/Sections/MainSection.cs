de5a88e667d320c09fee3b440a3c20f539701e62
kcbt123 <36097630+kcbt123@users.noreply.github.com>
2021-04-21 17:21:23 +0700

    Add Delete All function

--- Assets/Scripts/Sections/MainSection.cs
+++ Assets/Scripts/Sections/MainSection.cs
@@ -10,12 +10,18 @@ public class MainSection : MonoBehaviour
 
     private List<GameObject> listCodeBlock;
 
+    private GameObject scrollPanelObject;
+
     private VerticalLayoutGroup verticalLayoutGroup;
 
     //private List<CodeBlock> listCodeBlocks = new List<CodeBlock>(5);    // Start is called before the first frame update
     void Start()
     {
         verticalLayoutGroup = transform.GetComponent<VerticalLayoutGroup>();
+        scrollPanelObject = transform.GetChild(2).GetChild(0).GetChild(0).gameObject;
+        // Debug.Log("Object type 1" + transform.GetChild(2).gameObject.name);
+        // Debug.Log("Object type 2" + transform.GetChild(2).GetChild(0).gameObject.name);
+        // Debug.Log("Object type 3" + transform.GetChild(2).GetChild(0).GetChild(0).gameObject.name);
     }
 
     // Update is called once per frame
@@ -25,17 +31,26 @@ public class MainSection : MonoBehaviour
         {
             Debug.Log("Adding new code block");
             GameObject newCodeBlock = Instantiate(codeBlockPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-            newCodeBlock.transform.SetParent(this.transform);
+            newCodeBlock.transform.SetParent(scrollPanelObject.transform);
         } else if (Input.GetKeyDown(KeyCode.H))
         {
-            if (transform.childCount > 0) {
+            Transform trans = scrollPanelObject.transform;
+            if (trans.childCount > 0) {
                 Debug.Log("Deleting a code block");
-                GameObject lastCodeBlock = transform.GetChild(transform.childCount - 1).gameObject;
+                GameObject lastCodeBlock = trans.GetChild(trans.childCount - 1).gameObject;
                 Destroy(lastCodeBlock);
             } else {
                 Debug.Log("No child left");
             }
 
+        }
+    }
+
+    public void OnDeleteAllClick() {
+        if (scrollPanelObject.transform.childCount > 0) {
+            foreach (Transform child in scrollPanelObject.transform) {
+                Destroy(child.gameObject);
+            }
         }
     }
 }
